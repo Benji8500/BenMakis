b1eeee0496037c7c657de5a745d2fdc4f74d9b70
Mauricio Illanes <mauricioillanes17@gmail.com>
2020-09-14 23:50:56 -0400

    Inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,29 +10,6 @@ export class AppComponent implements OnInit {
 
   ngOnInit(){
 
-    const aux = [1,2,3,4,5,6,7,8];
-
-    const index = aux.findIndex(s => s === 3)
-    console.log(index);
-
-    const aux2 = aux.filter(s => s % 2 === 0)
-    console.log('Pares: ',aux2)
-
-    //spread operator = esparcidor de array, sirve con objetos json
-
-    const aux3 =[1000,2000,...aux];
-    console.log('Spread: ', aux3)
-
-    const aux4 ={
-      name: 'Mauricio',
-      lastName: 'Illens'
-    }
-
-    const aux5 = {
-      address: 'aaa',...aux4 
-    }
-    console.log('Spread2: ', aux5)
-
   }
 
 
